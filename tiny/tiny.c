d816290562ed6bb386aeeaf9607dea2b8f357327
iwannabeit <skzx737@naver.com>
2023-11-20 16:32:09 +0000

    mmap 을 malloc 으로

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -178,10 +178,13 @@ void serve_static(int fd, char *filename, int filesize)
 
   /* Send response body to client */
   srcfd = Open(filename, O_RDONLY, 0);
-  srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+  
+  //srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+  srcp = (char*)malloc(filesize);
+  rio_readn(srcfd, srcp, filesize);
   Close(srcfd);
   Rio_writen(fd, srcp, filesize);
-  Munmap(srcp, filesize);
+  free(srcp);
 }
 
 void get_filetype(char *filename,  char *filetype)
