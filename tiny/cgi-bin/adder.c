0b2fcb0af3434b80832bc7b1fab381558216da74
iwannabeit <skzx737@naver.com>
2023-11-21 08:24:03 +0000

    11.10 adder함수에 대한 html 형식 작성.

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -6,26 +6,36 @@
 
 int main(void) {
   //문자열을 저장할 버퍼 및 변수 선언
-  char *buf, *p;
+  char *buf, *p, *a, *b, *num1, *num2;
   char arg1 [MAXLINE], arg2[MAXLINE], content[MAXLINE];
   int n1=0, n2=0;
   
-  if((buf = getenv("QUERY_STRING")) != NULL){
+  if(((buf = getenv("QUERY_STRING")) != NULL) && strcmp(buf,"")){ //query_string --> ?이후에 나오는 문자열 즉, 100&100을 buf에 넣음
     p = strchr(buf, '&'); //buf에서 &가 처음으로 나타나는 위치를 가리키는 p
     *p = '\0'; //p를 null로 변경 -> buf문자열은 &문자를 기준으로 두 부분으로 나누어짐(&문자 이후부분은 새로운 문자열 시작)
-    strcpy(arg1, buf);
-    strcpy(arg2, p+1);
+    a = buf;
+    b = p+1;
+    num1 = strchr(a, '=');
+    *num1 = '\0';
+    num2 = strchr(b, '=');
+    *num2 = '\0';
+
+    strcpy(arg1, num1+1);
+    strcpy(arg2, num2+1);
     n1 = atoi(arg1);
     n2 = atoi(arg2);
-
   }
-
-  sprintf(content, "query_string=%s", buf);
-  sprintf(content, "welcome to add.com: ");
-  sprintf(content, "%sTHE Internet addition portal. \r\n<p>", content);
-  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1+n2);
-  sprintf(content, "%sThanks for visiting!\r\n", content);
-
+  
+  sprintf(content, "<html><head><title>Adder Form</title></head><body>\n");
+  sprintf(content + strlen(content), "<form action=\"/cgi-bin/adder\" method=\"get\">\n");
+  sprintf(content + strlen(content), "  <label for=\"num1\">Number 1:</label>\n");
+  sprintf(content + strlen(content), "  <input type=\"text\" id=\"num1\" name=\"num1\"><br>\n");
+  sprintf(content + strlen(content), "  <label for=\"num2\">Number 2:</label>\n");
+  sprintf(content + strlen(content), "  <input type=\"text\" id=\"num2\" name=\"num2\"><br>\n");
+  sprintf(content + strlen(content), "  <input type=\"submit\" value=\"Add\">\n");
+  sprintf(content + strlen(content), "</form>\n");
+  sprintf(content + strlen(content), "<p>The answer is: %d + %d = %d</p>\n", n1, n2, n1 + n2);
+  sprintf(content + strlen(content), "</body></html>\n");
   /*generate the http response*/
   printf("Connection: close\r\n");
   printf("Content-length: %d\r\n", (int)strlen(content));
